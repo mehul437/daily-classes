c086a730d3e89d06a77c94d05fe6859af12cd16d
mehul 437 <mehulkaushal.sq@gmail.com>
2022-02-23 20:56:56 +0530

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log("working");
+// const http = require("http")
+// const server  = http.createServer((request,response)=>{
+//     const student=[{name:"mehul",id:1},{name:"john",id:2}]
+  
+//     response.write("hello world")
+//     response.write("<h1>welcome</h1>")
+//     response.write(JSON.stringify(student))
+//     response.end();
+// })
+
+
+// server.listen(8000);
